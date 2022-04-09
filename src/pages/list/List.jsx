b32d5810961bc88a27f619c3d7aaeb9824467307
TegHocine <arcou98@hotmail.com>
2022-04-09 14:42:36 +0100

    feat: basic app skeleton

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import './List.scss'
+export default function List() {
+  return <div>List</div>
+}
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import './New.scss'
+
+export default function New() {
+  return <div>New</div>
+}
