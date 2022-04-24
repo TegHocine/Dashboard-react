ed7b67f115512efc85ae900b6f55856c546fbb15
TegHocine <arcou98@hotmail.com>
2022-04-24 03:01:42 +0100

    feat: data table with mui

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import SideBar from '../../components/sidebar/SideBar'
+import NavBar from '../../components/navbar/NavBar'
+import Datatable from '../../components/datatable/Datatable'
+
 import './List.scss'
 
 const List = () => {
-  return <div>List</div>
+  return (
+    <div className='list'>
+      <SideBar />
+      <div className='list__container'>
+        <NavBar />
+        <div className='datatable section'>
+          <Datatable />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default List
